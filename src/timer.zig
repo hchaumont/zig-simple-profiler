7dc358829d69ef42a0699378201f2cea9082beb4
Hans Chaumont <hans.chaumont@gmail.com>
2024-08-21 00:09:31 -0700

    Add function to estimate the CPU timer frequency

--- src/timer.zig
+++ src/timer.zig
@@ -19,3 +19,30 @@ test "reads time stamp counter" {
     const ts2 = rdtsc();
     try std.testing.expect(ts2 != ts1);
 }
+
+// Given a positive number of ms, estimates the frequency of the CPU time stamp
+// counter in Hz by comparing against the OS timer.
+export fn estimateCpuTimerFrequency(ms_to_wait: i64) u64 {
+    const us_to_wait: i64 = ms_to_wait * std.time.us_per_ms;
+
+    const cpu_tsc_start = rdtsc();
+    const os_timestamp_start = std.time.microTimestamp();
+
+    var os_time_elapsed: i64 = 0;
+    while (os_time_elapsed < us_to_wait) {
+        os_time_elapsed = std.time.microTimestamp() - os_timestamp_start;
+    }
+    const cpu_tsc_end = rdtsc();
+
+    const cpu_ticks_elapsed = cpu_tsc_end - cpu_tsc_start;
+    std.debug.assert(os_time_elapsed > 0);
+    const os_time_unsigned: u64 = @intCast(os_time_elapsed);
+    const cpu_timestamp_frequency = cpu_ticks_elapsed * std.time.us_per_s / os_time_unsigned;
+    return cpu_timestamp_frequency;
+}
+
+test "estimates CPU timer frequency" {
+    const ms_to_wait = 100;
+    const freq = estimateCpuTimerFrequency(ms_to_wait);
+    try std.testing.expect(freq > 0);
+}
