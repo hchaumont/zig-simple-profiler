c2423b702aac5d0abfdcd8b0829de5e2925e9162
Hans Chaumont <hans.chaumont@gmail.com>
2024-08-20 23:28:37 -0700

    Add function to call rdtsc instruction

--- src/timer.zig
+++ src/timer.zig
@@ -1,10 +1,21 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+// Refer to https://www.felixcloutier.com/x86/rdtsc
+pub inline fn rdtsc() u64 {
+    var hi: u32 = 0;
+    var low: u32 = 0;
+
+    asm (
+        \\rdtsc
+        : [low] "={eax}" (low),
+          [hi] "={edx}" (hi),
+    );
+    return (@as(u64, hi) << 32) | @as(u64, low);
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+test "reads time stamp counter" {
+    const ts1 = rdtsc();
+    const ts2 = rdtsc();
+    try std.testing.expect(ts2 != ts1);
 }
