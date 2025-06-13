78dea87085be001208dc1f567e5201758e00d4fa
Hans Chaumont <hans.chaumont@gmail.com>
2025-06-13 12:11:14 -0700

    Update for zig 0.14.1

--- build.zig
+++ build.zig
@@ -4,19 +4,22 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zig-tsc",
+    const lib_mod = b.createModule(.{
         .root_source_file = b.path("src/timer.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "zig-tsc",
+        .root_module = lib_mod,
+    });
+
     b.installArtifact(lib);
 
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/timer.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = lib_mod,
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
