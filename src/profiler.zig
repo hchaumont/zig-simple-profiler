bf71f357ff3359d706f2966f226e9ee9521edf65
Hans Chaumont <hans.chaumont@gmail.com>
2025-07-08 22:14:07 -0700

    Update profiler to take ZoneEnum as parameter

--- src/profiler.zig
+++ src/profiler.zig
@@ -7,101 +7,105 @@ const Anchor = struct {
     hit_count: u32,
 };
 
-pub const ZoneTag = enum {
-    functionA,
-    functionB,
+fn Profiler(comptime ZonesEnum: type) type {
+    return struct {
+        anchors: [@typeInfo(ZonesEnum).@"enum".fields.len]Anchor,
+        start_tsc: u64,
+        end_tsc: u64,
+        current_parent_index: ?usize,
 
-    fn getSize() usize {
-        return @typeInfo(ZoneTag).@"enum".fields.len;
-    }
-};
+        const Self = @This();
 
-const Profiler = struct {
-    anchors: [ZoneTag.getSize()]Anchor,
-    start_tsc: u64,
-    end_tsc: u64,
-    current_parent_index: ?usize,
+        pub fn startTiming(self: *Self) void {
+            self.start_tsc = timer.readCounter();
+        }
 
-    pub fn startTiming(self: *Profiler) void {
-        self.start_tsc = timer.readCounter();
-    }
+        pub fn stopTiming(self: *Self) void {
+            self.end_tsc = timer.readCounter();
+        }
 
-    pub fn stopTiming(self: *Profiler) void {
-        self.end_tsc = timer.readCounter();
-    }
-
-    pub fn printResults(self: *Profiler, printFreq: bool) !void {
-        const stdout = std.io.getStdOut().writer();
-        const total_cycles: u64 = self.end_tsc -% self.start_tsc;
-        try stdout.print("\nTotal cycles: {d}", .{total_cycles});
-        for (self.anchors, 0..) |a, i| {
-            const anchor_tag: ZoneTag = @enumFromInt(i);
-            const exclusive_pct: f64 = @as(f64, @floatFromInt(a.tsc_exclusive)) / @as(f64, @floatFromInt(total_cycles)) * 100;
-            try stdout.print("\n  {s} ({d} hits): {d:.2}%, {d} cycles", .{ @tagName(anchor_tag), a.hit_count, exclusive_pct, a.tsc_exclusive });
-            if (a.tsc_inclusive != a.tsc_exclusive) {
-                const inclusive_pct = @as(f64, @floatFromInt(a.tsc_inclusive)) / @as(f64, @floatFromInt(total_cycles)) * 100;
-                try stdout.print(" ({d:.2}% including children)", .{inclusive_pct});
+        pub fn printResults(self: *Self, printFreq: bool) !void {
+            const stdout = std.io.getStdOut().writer();
+            const total_cycles: u64 = self.end_tsc -% self.start_tsc;
+            try stdout.print("\nTotal cycles: {d}", .{total_cycles});
+            for (self.anchors, 0..) |a, i| {
+                const anchor_tag: ZonesEnum = @enumFromInt(i);
+                const exclusive_pct: f64 = @as(f64, @floatFromInt(a.tsc_exclusive)) / @as(f64, @floatFromInt(total_cycles)) * 100;
+                try stdout.print("\n  {s} ({d} hits): {d:.2}%, {d} cycles", .{ @tagName(anchor_tag), a.hit_count, exclusive_pct, a.tsc_exclusive });
+                if (a.tsc_inclusive != a.tsc_exclusive) {
+                    const inclusive_pct = @as(f64, @floatFromInt(a.tsc_inclusive)) / @as(f64, @floatFromInt(total_cycles)) * 100;
+                    try stdout.print(" ({d:.2}% including children)", .{inclusive_pct});
+                }
+            }
+            if (printFreq) {
+                const freq = timer.getCounterFrequency();
+                try stdout.print("\n(Estimated) Counter frequency: {d} Hz", .{freq});
             }
         }
-        if (printFreq) {
-            const freq = timer.getCounterFrequency();
-            try stdout.print("\n(Estimated) Counter frequency: {d} Hz", .{freq});
+
+        pub fn tag(self: *Self, comptime zone_tag: ZonesEnum) Block(ZonesEnum) {
+            _ = self;
+            const anchor_index = @intFromEnum(zone_tag);
+            return Block(ZonesEnum).init(anchor_index);
         }
-    }
+    };
+}
 
-    pub fn tag(self: *Profiler, comptime zone_tag: ZoneTag) Block {
-        _ = self;
-        const anchor_index = @intFromEnum(zone_tag);
-        return Block.init(anchor_index);
-    }
-};
-
-pub var GlobalProfiler = Profiler{
-    .anchors = .{Anchor{
-        .tsc_inclusive = 0,
-        .tsc_exclusive = 0,
-        .hit_count = 0,
-    }} ** ZoneTag.getSize(),
-    .start_tsc = undefined,
-    .end_tsc = undefined,
-    .current_parent_index = null,
-};
-
-const Block = struct {
-    prev_tsc_inclusive: u64,
-    start_tsc: u64,
-    anchor_index: usize,
-    parent_anchor_index: ?usize,
-
-    pub fn init(anchor_index: usize) Block {
-        const anchor: *Anchor = &GlobalProfiler.anchors[anchor_index];
-        const prev_tsc_inclusive = anchor.tsc_inclusive;
-        const parent_anchor_index: ?usize = GlobalProfiler.current_parent_index;
-        const current_tsc = timer.readCounter();
-
-        GlobalProfiler.current_parent_index = anchor_index;
-
-        return Block{
-            .prev_tsc_inclusive = prev_tsc_inclusive,
-            .start_tsc = current_tsc,
-            .anchor_index = anchor_index,
-            .parent_anchor_index = parent_anchor_index,
+pub fn ProfilerInstance(comptime ZonesEnum: type) *Profiler(ZonesEnum) {
+    const static = struct {
+        var instance: Profiler(ZonesEnum) = .{
+            .anchors = .{Anchor{
+                .tsc_inclusive = 0,
+                .tsc_exclusive = 0,
+                .hit_count = 0,
+            }} ** @typeInfo(ZonesEnum).@"enum".fields.len,
+            .start_tsc = undefined,
+            .end_tsc = undefined,
+            .current_parent_index = null,
         };
-    }
+    };
+    return &static.instance;
+}
 
-    pub fn deinit(self: *Block) void {
-        GlobalProfiler.current_parent_index = self.parent_anchor_index;
-        const total = timer.readCounter() -% self.start_tsc;
-        var anchor = &GlobalProfiler.anchors[self.anchor_index];
-        anchor.hit_count += 1;
-        anchor.tsc_inclusive = self.prev_tsc_inclusive +% total;
-        anchor.tsc_exclusive +%= total;
-        if (self.parent_anchor_index) |i| {
-            var parent_anchor = &GlobalProfiler.anchors[i];
-            parent_anchor.tsc_exclusive -%= total;
+fn Block(comptime ZonesEnum: type) type {
+    return struct {
+        prev_tsc_inclusive: u64,
+        start_tsc: u64,
+        anchor_index: usize,
+        parent_anchor_index: ?usize,
+
+        const Self = @This();
+
+        pub fn init(anchor_index: usize) Self {
+            const anchor: *Anchor = &ProfilerInstance(ZonesEnum).anchors[anchor_index];
+            const prev_tsc_inclusive = anchor.tsc_inclusive;
+            const parent_anchor_index: ?usize = ProfilerInstance(ZonesEnum).current_parent_index;
+            const current_tsc = timer.readCounter();
+
+            ProfilerInstance(ZonesEnum).current_parent_index = anchor_index;
+
+            return Self{
+                .prev_tsc_inclusive = prev_tsc_inclusive,
+                .start_tsc = current_tsc,
+                .anchor_index = anchor_index,
+                .parent_anchor_index = parent_anchor_index,
+            };
         }
-    }
-};
+
+        pub fn deinit(self: *Self) void {
+            ProfilerInstance(ZonesEnum).current_parent_index = self.parent_anchor_index;
+            const total = timer.readCounter() -% self.start_tsc;
+            var anchor = &ProfilerInstance(ZonesEnum).anchors[self.anchor_index];
+            anchor.hit_count += 1;
+            anchor.tsc_inclusive = self.prev_tsc_inclusive +% total;
+            anchor.tsc_exclusive +%= total;
+            if (self.parent_anchor_index) |i| {
+                var parent_anchor = &ProfilerInstance(ZonesEnum).anchors[i];
+                parent_anchor.tsc_exclusive -%= total;
+            }
+        }
+    };
+}
 
 const timer = struct {
     inline fn readCounter() u64 {
@@ -133,7 +137,7 @@ const timer = struct {
     }
 
     fn getCounterFrequency() u64 {
-        switch (builtin.cpu.arch) { 
+        switch (builtin.cpu.arch) {
             .x86_64 => {
                 // x86_x4 doesn't provide a way to read the counter frequency,
                 // so we estimate it by seeing how many ticks occur in 100ms
