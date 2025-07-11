02c0ec6c2e723d055bee7eaa19f9cba73f0ffc42
Hans Chaumont <hans.chaumont@gmail.com>
2025-07-10 22:05:27 -0700

    Simplify the example program

--- example/example.zig
+++ example/example.zig
@@ -12,30 +12,41 @@ const Profiler = profiler.ProfilerInstance(Zones);
 pub fn main() !void {
     Profiler.startTiming();
     var block = Profiler.tag(Zones.main);
-    defer block.deinit();
-    for (0..7) |i| {
-        functionA(i);
-    }
+
+    functionA(false);
+
+    // simulate 50 ms of work
+    std.Thread.sleep(std.time.ns_per_ms * 50);
+
+    functionA(true);
+
+    block.deinit();
     Profiler.stopTiming();
     try Profiler.printResults(true);
 }
 
-fn functionA(sleep_ms: u64) void {
+fn functionA(call_B: bool) void {
     var block = Profiler.tag(Zones.functionA);
     defer block.deinit();
+
     std.debug.print("functionA\n", .{});
-    std.Thread.sleep(std.time.ns_per_ms * sleep_ms);
-    if (sleep_ms % 2 == 0) {
-        functionB(sleep_ms);
+    // simulate 200ms worth of work
+    std.Thread.sleep(std.time.ns_per_ms * 200);
+
+    if (call_B) {
+        functionB(true);
     }
 }
 
-fn functionB(sleep_ms: u64) void {
+fn functionB(call_A: bool) void {
     var block = Profiler.tag(Zones.functionB);
     defer block.deinit();
+
     std.debug.print("functionB\n", .{});
-    std.Thread.sleep(std.time.ns_per_ms * sleep_ms);
-    if (sleep_ms > 0 and sleep_ms % 3 == 0) {
-        functionA(sleep_ms / 6);
+    // simulate 100 ms of work
+    std.Thread.sleep(std.time.ns_per_ms * 100);
+
+    if (call_A) {
+        functionA(false);
     }
 }
